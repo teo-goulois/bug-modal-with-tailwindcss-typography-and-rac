a819bbfd91e41f0527581f2c6be2e7eaa4c416ba
Téo Goulois <teo.goulois.dev@gmail.com>
2025-08-27 18:14:31 +0200

    Add @tanstack/react-query for data fetching and create Providers component
    - Added `@tanstack/react-query` as a dependency in `package.json` and updated `pnpm-lock.yaml`.
    - Created a new `Providers` component to wrap the application with `NuqsAdapter`, `QueryClientProvider`, and `I18nProvider` for improved state management and internationalization.
    - Updated `RootLayout` to use the new `Providers` component instead of `NuqsAdapter` directly.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SharedNavbar } from "@/components/shared/shared-navbar";
 import { NuqsAdapter } from "nuqs/adapters/react";
+import { Providers } from "@/components/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,10 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NuqsAdapter>
+        <Providers>
           <SharedNavbar />
           {children}
-        </NuqsAdapter>
+        </Providers>
       </body>
     </html>
   );
---
+++ src/components/providers.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import { NuqsAdapter } from "nuqs/adapters/react";
+import { useRouter } from "next/navigation";
+import { PropsWithChildren, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { I18nProvider, RouterProvider } from "react-aria-components";
+
+type Props = {};
+
+export const Providers = ({ children }: PropsWithChildren<Props>) => {
+  const router = useRouter();
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <NuqsAdapter>
+      <QueryClientProvider client={queryClient}>
+        <I18nProvider locale="fr">
+          <RouterProvider navigate={router.push}>{children}</RouterProvider>
+        </I18nProvider>
+      </QueryClientProvider>
+    </NuqsAdapter>
+  );
+};
+
+{
+  /* <AuthProvider session={session}>
+        <DesignSystemProviders navigate={router.push} locale="fr">
+          <GleapProvider />
+          <TRPCReactProvider>
+            <FavoriteModalProvider>{children}</FavoriteModalProvider>
+            <ReactQueryDevtools />
+          </TRPCReactProvider>
+        </DesignSystemProviders>
+      </AuthProvider> */
+}
