aacffe02f2882321cffa40e7f6272a681d76a9c5
Téo Goulois <teo.goulois.dev@gmail.com>
2025-08-28 13:51:20 +0200

    Wrap SharedNavbar and children in Providers component for improved context management
    - Reintroduced the Providers component in RootLayout to encapsulate SharedNavbar and children, ensuring proper context for routing and internationalization.
    - Simplified Providers component by removing the QueryClientProvider, enhancing clarity while maintaining functionality.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SharedNavbar />
-        {children}
+        <Providers>
+          <SharedNavbar />
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,21 +1,17 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { PropsWithChildren, useState } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PropsWithChildren } from "react";
 import { I18nProvider, RouterProvider } from "react-aria-components";
 
 type Props = {};
 
 export const Providers = ({ children }: PropsWithChildren<Props>) => {
   const router = useRouter();
-  const [queryClient] = useState(() => new QueryClient());
   return (
-    <QueryClientProvider client={queryClient}>
-      <I18nProvider locale="fr">
-        <RouterProvider navigate={router.push}>{children}</RouterProvider>
-      </I18nProvider>
-    </QueryClientProvider>
+    <I18nProvider locale="fr">
+      <RouterProvider navigate={router.push}>{children}</RouterProvider>
+    </I18nProvider>
   );
 };
 
