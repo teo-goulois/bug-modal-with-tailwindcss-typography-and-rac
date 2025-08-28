86b59b40d322a99eee2744eaa9e6024c164a3532
Téo Goulois <teo.goulois.dev@gmail.com>
2025-08-28 13:52:36 +0200

    Refactor Providers component to remove I18nProvider
    - Simplified the Providers component by removing the I18nProvider, focusing solely on the RouterProvider for navigation.
    - Enhanced clarity and maintainability of the component structure.

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,27 +2,11 @@
 
 import { useRouter } from "next/navigation";
 import { PropsWithChildren } from "react";
-import { I18nProvider, RouterProvider } from "react-aria-components";
+import { RouterProvider } from "react-aria-components";
 
 type Props = {};
 
 export const Providers = ({ children }: PropsWithChildren<Props>) => {
   const router = useRouter();
-  return (
-    <I18nProvider locale="fr">
-      <RouterProvider navigate={router.push}>{children}</RouterProvider>
-    </I18nProvider>
-  );
+  return <RouterProvider navigate={router.push}>{children}</RouterProvider>;
 };
-
-{
-  /* <AuthProvider session={session}>
-        <DesignSystemProviders navigate={router.push} locale="fr">
-          <GleapProvider />
-          <TRPCReactProvider>
-            <FavoriteModalProvider>{children}</FavoriteModalProvider>
-            <ReactQueryDevtools />
-          </TRPCReactProvider>
-        </DesignSystemProviders>
-      </AuthProvider> */
-}
