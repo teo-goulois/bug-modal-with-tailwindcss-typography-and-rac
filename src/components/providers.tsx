bec302cc9932408d9399ec50915ecbc25059d4d6
Téo Goulois <teo.goulois.dev@gmail.com>
2025-08-28 14:41:46 +0200

    Add routerOptions to RouterConfig in react-aria-components module
    - Extended the RouterConfig interface to include routerOptions, ensuring type safety for the push method's parameters.
    - This change enhances type inference and improves the overall clarity of the routing configuration in the application.

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -6,6 +6,14 @@ import { RouterProvider } from "react-aria-components";
 
 type Props = {};
 
+declare module "react-aria-components" {
+  interface RouterConfig {
+    routerOptions: NonNullable<
+      Parameters<ReturnType<typeof useRouter>["push"]>[1]
+    >;
+  }
+}
+
 export const Providers = ({ children }: PropsWithChildren<Props>) => {
   const router = useRouter();
   return <RouterProvider navigate={router.push}>{children}</RouterProvider>;
