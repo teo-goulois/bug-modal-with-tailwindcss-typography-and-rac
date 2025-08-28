4925cf413951cead4c4e453b8d41d8884b3713a3
Téo Goulois <teo.goulois.dev@gmail.com>
2025-08-28 12:22:49 +0200

    Refactor SharedModal to remove unused hooks and streamline input handling
    - Removed `useRouter` and `useQueryState` hooks as they were not utilized in the component.
    - Eliminated the search input field and its associated state management for a cleaner modal interface.
    - Updated the modal structure to enhance clarity and maintainability.

--- src/components/shared/shared-modal.tsx
+++ src/components/shared/shared-modal.tsx
@@ -1,18 +1,13 @@
 "use client";
 
-import { useDisclosure, UseDisclosureProps } from "@/hooks/use-disclosure";
+import { UseDisclosureProps } from "@/hooks/use-disclosure";
 import { Modal } from "../ui/modal";
 import { twJoin } from "tailwind-merge";
-import { useRouter } from "next/navigation";
-import { parseAsString, useQueryState } from "nuqs";
 import { Link } from "../ui/link";
 
 type Props = {} & UseDisclosureProps;
 
 export const SharedModal = ({ ...disclosureProps }: Props) => {
-  const router = useRouter();
-  const [search, setSearch] = useQueryState("q", parseAsString);
-
   const links = [
     {
       href: "/",
@@ -49,28 +44,14 @@ export const SharedModal = ({ ...disclosureProps }: Props) => {
         className={twJoin(
           "max-h-[90dvh] md:w-[calc(100dvw-1rem)] md:!max-w-none md:h-[calc(100dvh-1rem)] md:max-h-screen"
         )}
-        //side="bottom"
-        /*  classNames={{
-          content: cn(
-            "max-h-[90dvh] md:h-[calc(100dvh-1rem)] md:max-h-screen",
-            //inputValue?.length > 0 ? "h-[90dvh] md:h-screen" : "",
-          ),
-        }} */
       >
         <Modal.Body className="space-y-2.5 h-full min-h-40 flex-col ">
-          <input
-            className="border"
-            type="text"
-            value={search ?? ""}
-            onChange={(e) => setSearch(e.target.value)}
-          />
           {links.map((link) => (
             <Link
               key={link.href}
               href={link.href}
               onPress={() => {
                 disclosureProps.onClose?.();
-                setSearch(null);
               }}
             >
               {link.label}
