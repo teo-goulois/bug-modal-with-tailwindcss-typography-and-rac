de30ebc89cc91b8f9a91e97cda005cdbd9ef9a15
Téo Goulois <teo.goulois.dev@gmail.com>
2025-08-28 11:31:54 +0200

    a

--- src/app/page-2/page.tsx
+++ src/app/page-2/page.tsx
@@ -12,30 +12,46 @@ export default async function Page({
   params: Promise<{ slug: string }>;
   searchParams: Promise<Record<string, string | string[] | undefined>>;
 }) {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const fakePromise = new Promise((resolve) => {
+    setTimeout(() => {
+      const contente = `
+      <h1>⚔️ L&rsquo;Irr&eacute;ductible Festival 2026 &agrave; Quimper</h1>
+<p>Hey toi ! Pr&ecirc;t&middot;e &agrave; vivre une exp&eacute;rience unique du <strong>26 au 28 juin 2026</strong>&nbsp;&agrave; <strong>Quimper</strong>, au <strong>Chemin de K&eacute;rourien</strong> ? Trois jours de musique, de fun et d&rsquo;ambiance bretonne d&eacute;jant&eacute;e t&rsquo;attendent dans un cadre convivial et chaleureux 🏰🎶.</p>
+<h2>📅 Les infos cl&eacute;s</h2>
+<ul>
+<li><strong>Quand &amp; o&ugrave; ?</strong> Du <strong>26 au 28 juin 2026</strong>&nbsp;&agrave; Quimper, au Chemin de K&eacute;rourien 📍</li>
+<li><strong>Ambiance</strong> : festival &agrave; taille humaine, esprit village gaulois, convivial et fun &agrave; souhait 🎉⚔️</li>
+<li><strong>Camping</strong>&nbsp;: <strong>gratuit</strong> et r&eacute;serv&eacute; aux festivaliers ⛺💚</li>
+<li style="font-weight: bold;"><strong>Parking v&eacute;hicule am&eacute;nag&eacute; gratuit !</strong></li>
+</ul>
+<h2>⏰ Horaires &amp; acc&egrave;s pratiques</h2>
+<ul>
+<li>Festival ouvert tous les jours, avec une programmation qui d&eacute;chire de jour comme de nuit 🌞🌙</li>
+<li>Bonus chapiteau &eacute;lectro avec un d&eacute;cor de folie pour te faire danser non-stop de 21h &agrave; 3h 🎧🕺💃</li>
+</ul>
+<h2>Pourquoi tu dois y &ecirc;tre ?</h2>
+<ol>
+<li><strong>Ambiance Gauloise &amp; bretonne</strong> : d&eacute;connexion totale et bonne humeur garantie ⚔️😄</li>
+<li><strong>Prog&rsquo; vari&eacute;e</strong> : &eacute;lectro, swing, rap, rock, folk&hellip; il y en a pour tous les go&ucirc;ts 🎸🎷🎤</li>
+<li><strong>Festival &agrave; taille humaine</strong> : convivialit&eacute; et partage avant tout 👥🤝</li>
+<li><strong>Camping gratuit</strong> : pour vivre l&rsquo;exp&eacute;rience compl&egrave;te sans stress ⛺💚</li>
+</ol>
+<h3>Tu veux un aper&ccedil;u ? Regardes l'<a href="https://www.irreductible-festival.fr/" target="_blank" rel="noopener"><strong>aftermovie de l'&eacute;dition 2025</strong></a> !</h3>
+<p><!-- notionvc: b5e1d34c-c396-497b-8090-025607c1e447 --></p>
+`;
+
+      resolve(contente);
+    }, 1000);
+  });
+  const content = (await fakePromise) as string;
+  console.log(content);
   return (
     <div>
       <p>My Async Page 2</p>
       <div
         className="prose"
         dangerouslySetInnerHTML={{
-          __html: `
-          <h3 data-olk-copy-source="MessageBody">🎂 Birthday Party &ndash; Samedi 27 septembre 🥳</h3>
-<div>&Ccedil;a y est&hellip; Vertical'Art souffle sa premi&egrave;re bougie ! 🥲</div>
-<div>Et franchement, impossible de ne pas marquer le coup.</div>
-<div>Ils ont donc pr&eacute;par&eacute; un menu sportif &amp; festif 🤩</div>
-<div>📍 RDV d&egrave;s 15h00 pour :</div>
-<ul>
-<li>🧗 Mini contest et challenges en &eacute;quipe.</li>
-<li>🎶 Concert de Swansea : parce qu&rsquo;apr&egrave;s grimper, on danse.</li>
-<li>🎟 Tombola : des cadeaux &agrave; gogo.</li>
-<li>🔥 BBQ + dessert surprise (oui, vous allez aimer).</li>
-</ul>
-<p>Ils comptent sur vous pour venir vous d&eacute;fouler, vous r&eacute;galer et rigoler !</p>
-<p data-start="55" data-end="305"><strong>Vertical'Art </strong>est un lieu de vie centr&eacute; autour de l'<strong data-start="113" data-end="125">escalade</strong> 🧗&zwj;♂️ : <strong data-start="134" data-end="144">1000m&sup2;</strong> de blocs adapt&eacute;s &agrave; tous, sur des murs aux profils vari&eacute;s et atypiques, ainsi qu'un espace <strong data-start="250" data-end="277">renforcement musculaire</strong> 💪 et un <strong data-start="287" data-end="296">sauna</strong> 🧖&zwj;♀️.</p>
-<p data-start="55" data-end="305">Pour prolonger le plaisir, un&nbsp;<strong data-start="340" data-end="354">restaurant</strong> &agrave; la d&eacute;coration chaleureuse, bas&eacute; sur une cuisine <strong data-start="414" data-end="424">maison</strong>, <strong data-start="429" data-end="439">locale</strong> et <strong data-start="446" data-end="459">de saison</strong> 🌸🍂.</p>
-<p data-start="469" data-end="560">Enfin, une <strong data-start="480" data-end="490">&eacute;quipe</strong> pr&ecirc;te &agrave; vous accueillir avec <strong data-start="523" data-end="531">joie</strong> 😄 et <strong data-start="538" data-end="554">bonne humeur</strong> 🌞.</p>
-`,
+          __html: content,
         }}
       />
     </div>
