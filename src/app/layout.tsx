9dbb49ceaf16f776344fb143f0909a3758f7b468
Téo Goulois <teo.goulois.dev@gmail.com>
2025-08-28 13:49:46 +0200

    Refactor RootLayout to remove Providers wrapper
    - Simplified the RootLayout component by removing the unnecessary Providers wrapper around SharedNavbar and children.
    - This change enhances the layout structure and improves readability.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,10 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>
-          <SharedNavbar />
-          {children}
-        </Providers>
+        <SharedNavbar />
+        {children}
       </body>
     </html>
   );
